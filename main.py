182c11b21ec7e0c08d17cd275d1d7debd27cc6a5
raghavbakshi <64217734+raghavbakshi@users.noreply.github.com>
2022-06-03 13:46:14 +0530

    final commit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from db_obs import sql
+import datetime
+
 
 # Basic setup
 o1=sql('localhost','root','password','ineuron')
@@ -18,3 +20,56 @@ o1.insert_BankAccountTable((2,"PNB",14,True,5500))
 o1.insert_BankAccountTable((3,"PNB",12,False,65000))
 o1.insert_BankAccountTable((4,"JKB",124,True,3000))
 
+# Check the current account balance
+def get_acc_bal(ID):
+    mydb = connection.connect(host="localhost", user="root", passwd="password", use_pure=True)
+    cursor = mydb.cursor()
+    query='SELECT Amount FROM ineuron.BankAccountTable where userId = {}'.format(ID)
+    cursor.execute(query)
+
+    (i for i in cursor)
+    return(i[0])
+
+get_acc_bal(1)
+
+
+# Withraw the amount
+def withraw(ID,bank_acc_id,input_amount):
+    mydb = connection.connect(host="localhost", user="root", passwd="password", use_pure=True)
+    cursor = mydb.cursor()
+    query='SELECT Amount FROM ineuron.BankAccountTable where userId = {}'.format(ID)
+    cursor.execute(query)
+    for i in cursor:
+        amount_present = int(i[0])
+    if amount_present <= 5000:
+        return("Minimum balance of 5000 should be maintained")
+    elif input_amount > amount_present:
+        return("Insufficient Balance")
+    else:
+        updated_amount = amount_present - input_amount
+        mydb = connection.connect(host="localhost", user="root", passwd="password", use_pure=True)
+        cursor = mydb.cursor()
+        query='UPDATE ineuron.BankAccountTable SET Amount = {} where userId = {}'.format(updated_amount,ID)
+        cursor.execute(query)
+        mydb.commit()
+        o1.insert_TransactionTable((ID,bank_acc_id,input_amount))
+    return "amount left in your account is: ",updated_amount
+
+withraw(2,14,500)
+
+# Account Statement
+
+def statement(start,end,id):
+    mydb = connection.connect(host="localhost", user="root", passwd="password", use_pure=True)
+    cursor = mydb.cursor()
+    query='SELECT * FROM ineuron.TransactionTable where transactionDate <= {} and transactionDate >= {} and userId = {} '.format(end,start,ID)
+    cursor.execute(query)
+    for i in cursor:
+        print(i)    
+
+
+
+
+"""
+   END
+    """
